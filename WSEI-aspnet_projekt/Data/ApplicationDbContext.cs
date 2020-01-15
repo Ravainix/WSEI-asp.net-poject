eb367d083c5dfa8a1a77d2835c249e614c81c83a
Ravainix <ravainix@gmail.com>
2020-01-15 22:25:37 +0100

    Initial commit

---
+++ WSEI-aspnet_projekt/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using WSEI_aspnet_projekt.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSEI_aspnet_projekt.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
