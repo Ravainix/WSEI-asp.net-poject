90cb0910201211d6a9d453cad446e254baa5af2f
Ravainix <ravainix@gmail.com>
2020-01-19 20:11:28 +0100

    Add Sass

--- WSEI-aspnet_projekt/ClientApp/src/index.js
+++ WSEI-aspnet_projekt/ClientApp/src/index.js
@@ -5,6 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
 
+import './stylesheets/main.scss';
+
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
