12d134e8ed2ca1a2a73116081e1c634a202cb9f5
Ravainix <ravainix@gmail.com>
2020-01-21 01:51:57 +0100

    Add API helpers

---
+++ WSEI-aspnet_projekt/ClientApp/src/helpers/recipesApi.js
@@ -0,0 +1,14 @@
+﻿import * as api from './api'
+import { recipesApiURL } from './routes'
+
+//get all recipes 
+export const getAll = () => api.get(recipesApiURL())
+
+//create new recipe
+export const create = (params) => api.post(recipesApiURL(), { ...params })
+
+//update existing recipe 
+export const update = (id, params) => api.put(recipesApiURL(id), { ...params  })
+
+//delete existing recipe
+export const destroy = (id) => api.destroy(recipesApiURL(id))
