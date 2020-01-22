d978f319e6d710f92e140686bb850b2782de4402
Ravainix <ravainix@gmail.com>
2020-01-22 21:53:26 +0100

    Add auth token to request

--- WSEI-aspnet_projekt/ClientApp/src/components/CurrentUserTest.js
+++ WSEI-aspnet_projekt/ClientApp/src/components/CurrentUserTest.js
@@ -1,9 +1,12 @@
 ﻿import React from 'react'
 import { Button } from 'reactstrap';
+import AuthorizeService from './api-authorization/AuthorizeService'
 
 const DestroyRecipe = ({ id }) => {
-    const handleClick = (id) => {
-        fetch('/api/currentUserRecipes')
+    const handleClick = async (id) => {
+        const user = await AuthorizeService.getAccessToken()
+        console.log(user)
+        fetch('/api/currentUserRecipes', { headers: { Authorization: "Bearer " + user}})
             .then(response => response.json())
             .then(json => console.log(json))
             .catch(err => console.error(err))
