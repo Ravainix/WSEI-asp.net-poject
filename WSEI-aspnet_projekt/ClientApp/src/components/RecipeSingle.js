56f7a077436ad57fc7cc65295aa93fa0a47e4758
Ravainix <ravainix@gmail.com>
2020-09-20 01:26:39 +0200

    Fix ingredients display

--- WSEI-aspnet_projekt/ClientApp/src/components/RecipeSingle.js
+++ WSEI-aspnet_projekt/ClientApp/src/components/RecipeSingle.js
@@ -1,9 +1,7 @@
 ﻿import React, { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom";
-import { get } from '../helpers/recipesApi'
+import { getWithIngredients } from '../helpers/recipesApi'
 
-
-import { useSelector } from 'react-redux'
 import { Row, Col } from 'reactstrap'
 import Ingredient from './Ingredient'
 import ShareRecipe from './ShareRecipe'
@@ -12,24 +10,24 @@ import { faUserFriends, faHourglass } from '@fortawesome/free-solid-svg-icons'
 
 const RecipeSingle = () => {
     const { id } = useParams();
-    const [recipe, setRecipe] = useState(null)
+    const [data, setData] = useState(null)
 
    // const recipe = useSelector(state => state.recipes.entities.find(el => el.id === id))
 
     useEffect(() => {
         const fetchData = async () => {
-            get(id).then( (data) => 
-            setRecipe(data))
+            getWithIngredients(id).then( (data) => 
+            setData(data))
         }
         fetchData()
     }, [])
 
     return (
-        recipe ? (
+        data ? (
             <>
                 <Row className="singleRecipe-image">
                     <Col>
-                        <img  src="http://michalwrzosek.pl/wp-content/uploads/2020/07/bok-5.jpg" alt={recipe.name} />
+                        <img src={data.recipe.image} alt={data.recipe.name} />
                     </Col>
                 </Row>
                 <Row className="mt-2 align-items-center">
@@ -41,12 +39,12 @@ const RecipeSingle = () => {
                     <Col xs='12' sm='6'>
                         <Row className="singleRecipe-heading">
                             <Col>
-                                <h2>{recipe.name}</h2>
+                                <h2>{data.recipe.name}</h2>
                             </Col>
                         </Row>
                         <Row className="singleRecipe-description my-3">
                             <Col>
-                                <p>{recipe.description}</p>
+                                <p>{data.recipe.description}</p>
                             </Col>
                         </Row>
                         <Row className="singleRecipe-preparation ">
@@ -56,7 +54,7 @@ const RecipeSingle = () => {
                         </Row>
                         <Row className="singleRecipe-preparation my-3">
                             <Col>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                <p></p>
                             </Col>
                         </Row>
                     </Col>
@@ -74,9 +72,7 @@ const RecipeSingle = () => {
                             </div>
                         </div>
                         <ul style={{ userSelect: 'none'}}>
-                            <Ingredient>{recipe.description}</Ingredient>
-                            <Ingredient>{recipe.description}</Ingredient>
-                            <Ingredient>{recipe.description}</Ingredient>
+                            {data.ingredients.map(({name, amount}) => <Ingredient key={name}>{name} <span style={{color: "#8C8B8B"}}>({amount})</span></Ingredient>)}
                         </ul>
 
                     </Col>
